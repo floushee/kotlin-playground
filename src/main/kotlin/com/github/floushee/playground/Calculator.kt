e995f2e79ce9827a9a59ef6489b566418a31d6b8
floushee <dominik.deutsch@gmail.com>
2020-01-15 20:17:45 +0100

    Implementation of division calculation

--- src/main/kotlin/com/github/floushee/playground/Calculator.kt
+++ src/main/kotlin/com/github/floushee/playground/Calculator.kt
@@ -6,4 +6,4 @@ fun multiply(a: Double, b: Double): Double = a * b
 
 fun subtract(a: Double, b:Double): Double = a - b
 
-fun divide(a: Double, b:Double): Double = TODO()
+fun divide(a: Double, b:Double): Double = a / b
